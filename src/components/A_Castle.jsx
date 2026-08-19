0d2435d18e57b580dc17ed99e3c8efae80236b59
อัลเบิร์ส ผลบุตร <albert.phonbut@gmail.com>
2026-08-19 13:26:42 +0700

    pokemon first commit

--- src/components/A_Castle.jsx
+++ src/components/A_Castle.jsx
@@ -1,13 +1,27 @@
 import Tower from "./B_Tower";
 
-export default function A_Castle({question, answer, handleAnswer}) {
-    return (
-        <div className="bg-red-100 shadow-md text-center">
-            <h1>Castle
-            </h1>
-    
-            <Tower question={question} answer={answer} handleAnswer={handleAnswer}/>
-        </div>
-        
-    );
-} 
+export default function A_Castle({
+  question,
+  answer,
+  handleAnswer,
+  pokemonCage,
+  pokemon,
+  pokemonName,
+  pokemonOption,
+}) {
+  return (
+    <div className="bg-red-100 shadow-md text-center">
+      <h1>Castle</h1>
+
+      <Tower
+        question={question}
+        answer={answer}
+        handleAnswer={handleAnswer}
+        pokemonCage={pokemonCage}
+        pokemon={pokemon}
+        pokemonName={pokemonName}
+        pokemonOption={pokemonOption}
+      />
+    </div>
+  );
+}
--- src/components/F_Corridor.jsx
+++ src/components/F_Corridor.jsx
@@ -1,11 +1,29 @@
 import Gallery from "./G_Gallery";
 
-export default function Corridor({question, answer, handleAnswer}) {
-    return(
-        <div className="bg-stone-100 shadow-md pl-1 pr-1">
-            <h1>Corridor</h1>
-            
-            <div><Gallery question={question} answer={answer} handleAnswer={handleAnswer}/></div>
-        </div>
-);
+export default function Corridor({
+  question,
+  answer,
+  handleAnswer,
+  pokemonCage,
+  pokemon,
+  pokemonName, 
+  pokemonOption,
+}) {
+  return (
+    <div className="bg-stone-100 shadow-md pl-1 pr-1">
+      <h1>Corridor</h1>
+
+      <div>
+        <Gallery
+          question={question}
+          answer={answer}
+          handleAnswer={handleAnswer}
+          pokemonCage={pokemonCage}
+          pokemon={pokemon}
+          pokemonName={pokemonName}
+          pokemonOption={pokemonOption}
+        />
+      </div>
+    </div>
+  );
 }
